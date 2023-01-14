92e6a62ee8a32a54a924f6f1693fde75cab65425
clairevillan <elishaclaire80@gmail.com>
2023-01-14 11:56:02 -0500

    fixed pfp

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 const img = document.getElementById("pfp");
 
 let toggle = true;
+let slideIndex = 1;
+showSlides(slideIndex);
 
 img.addEventListener('click', function(){
 
     toggle = !toggle;
 
     if(toggle){
-        img.src = "./resources/images/pfp.jfif";
+        img.src = "./resources/images/pfp.jpg";
     } else {
         img.src = "./resources/images/siopao.jfif";
     }
     
 })
+
+// controlling next and previous actions
+function plusSlides(n){
+    showSlides(slideIndex += n);
+}
+
